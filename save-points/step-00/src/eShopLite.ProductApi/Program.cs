2fcce4023ede0fa3b7074a099e0ccf34ce309b12
Justin Yoo <justin.yoojh@gmail.com>
2024-11-25 23:41:28 +1100

    Add save point for step 00

---
+++ save-points/step-00/src/eShopLite.ProductApi/Program.cs
@@ -0,0 +1,43 @@
+using eShopLite.ProductApi.Endpoints;
+using eShopLite.ProductApi.Extensions;
+using eShopLite.ProductData;
+
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddProblemDetails();
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ProductDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("ProductsContext") ?? throw new InvalidOperationException("Connection string 'ProductsContext' not found.");
+    options.UseSqlite(connectionString);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    // Redirect to OpenAPI endpoint
+    app.MapGet("/", () =>
+    {
+        return Results.Redirect("/openapi/v1.json");
+    })
+    .ExcludeFromDescription();
+}
+
+app.UseHttpsRedirection();
+
+app.MapProductEndpoints();
+
+app.CreateDbIfNotExists();
+
+app.Run();
